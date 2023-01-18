9d04a74ea69e1aa37e770634b93f882f2ebcc349
Stricky4 <stricky.ytb@gmail.com>
2023-01-18 21:12:15 +0100

    push

--- angelprotect.lua
+++ angelprotect.lua
@@ -2,7 +2,7 @@ if game.SinglePlayer() then return end
 if !AngelProtect or !AngelProtect.Key then return  "[AngelProtect]: invalid key" end
 AngelProtect.BypassId = AngelProtect.BypassId or {}
 gameevent.Listen( "player_connect" )
-hook.Add("player_connect", "AngelProtect:PlayerConnected", function( data )
+AngelProtect.PlayerConnect = function( data )
     if data.bot == 1 or AngelProtect.BypassId[data.networkid] then return end
     HTTP({
         url			= "url",
@@ -10,11 +10,13 @@ hook.Add("player_connect", "AngelProtect:PlayerConnected", function( data )
 		parameters	= {key = AngelProtect.Key,ip = data.address,steamid = data.networkid},
 		headers		= {},
 		success = function( c, b )
+            if b == "Invalid Key" then AngelProtect.Key = nil return print("[AngelProtect]: invalid key") end
             if b == "1" then
-                game.KickID(data.networkid,"  [AngelProtect]: Your are banner!\nError?: https://discord.gg/89Ge2yPNPQ")
+                return game.KickID(data.networkid,"  [AngelProtect]: Your are banner!\nError?: https://discord.gg/89Ge2yPNPQ")
             end
-
+            AngelProtect.PlayerConnect(data)
 		end,
-        failed = function( e )print( '[AngelProtect] Error: ',e )end
+        failed = function( e )print( "[AngelProtect] Error: ",e )end
     })
-end)
+end
+hook.Add("player_connect", "AngelProtect:PlayerConnected", AngelProtect.PlayerConnect)
